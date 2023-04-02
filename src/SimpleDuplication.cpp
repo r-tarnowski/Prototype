09ab936339d5a3f121f0f7246e8ab1e216cffefe
r-tarnowski <radek.github@wp.pl>
2023-04-02 23:25:03 +0200

    Initial commit

---
+++ src/SimpleDuplication.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+
+using std::cout;
+using std::endl;
+
+void printHeader() {
+   cout << endl;
+   cout << "================================================" << endl;
+   cout << "Design Patterns in Modern C++ by Dmitri Nesteruk" << endl;
+   cout << "Prototype, Simple Duplication                   "<< endl;
+   cout << "================================================" << endl;
+   cout << endl;
+}
+
+
+
+int main(int argc, char *argv[]) {
+   printHeader();
+
+   cout << endl;
+   return 0;
+}
